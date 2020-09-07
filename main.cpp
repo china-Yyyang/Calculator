dee663ce1659bd1bad69dd65ea5ad95dd62b1778
KEVIN <43997638+china-Yyyang@users.noreply.github.com>
2020-09-07 19:49:02 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     //setTitle
-    w.setWindowTitle("Qcounter");
+    w.setWindowTitle("Calcualtor");
 
     w.show();
 
